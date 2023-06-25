9669771550f46bb2b744614f1cc8424cf4e6394e
Nicoli <nicolas.li@hotmail.fr>
2023-06-25 16:19:42 +0200

    fix: always choose a different element in permutation (#6)

--- src/hooks/useDegreePermutation.ts
+++ src/hooks/useDegreePermutation.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useInterval } from "./useInterval";
 import { type Chord } from "./useAvailableChords";
 import { type Degree } from "./useAvailableDegrees";
@@ -9,6 +9,35 @@ function getRandomElement<T>(array: T[]) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex] as T;
 }
+
+function selectNextElement<T>(array: T[], previousElement: T): T {
+  // TODO: Empty array case not taken into account
+  if (array.length === 1 && array[0]) {
+    return array[0];
+  }
+  const randomElement = getRandomElement(array);
+  // If the same element is chosen, rerun the function
+  if (randomElement === previousElement) {
+    return selectNextElement(array, previousElement);
+  }
+  return randomElement;
+}
+
+const usePauseWhenEmptyAvailableChordsOrDegrees = ({
+  availableDegrees,
+  availableChords,
+  setIsPaused,
+}: {
+  availableChords: Chord[];
+  availableDegrees: Degree[];
+  setIsPaused: (isPaused: boolean) => void;
+}) => {
+  useEffect(() => {
+    if (availableDegrees.length === 0 || availableChords.length === 0) {
+      setIsPaused(true);
+    }
+  }, [availableDegrees, availableChords]);
+};
 
 export const useDegreePermutation = ({
   availableDegrees,
@@ -21,14 +50,20 @@ export const useDegreePermutation = ({
   const [noteDegree, setNoteDegree] = useState<string>("1");
   const [isPaused, setIsPaused] = useState(true);
 
+  usePauseWhenEmptyAvailableChordsOrDegrees({
+    availableDegrees,
+    availableChords,
+    setIsPaused,
+  });
+
   const startStopPermutation = useCallback(() => {
     setIsPaused(!isPaused);
   }, [setIsPaused, isPaused]);
 
   useInterval(
     () => {
-      const randomChordDegree = getRandomElement(availableChords);
-      const randomNoteDegree = getRandomElement(availableDegrees);
+      const randomChordDegree = selectNextElement(availableChords, chordDegree);
+      const randomNoteDegree = selectNextElement(availableDegrees, noteDegree);
       setChordDegree(randomChordDegree);
       setNoteDegree(randomNoteDegree);
     },
