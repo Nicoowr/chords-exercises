5a6cb65fbb14464aab6932213b1850197e720669
nicoowr <nicolas@theforecastingcompany.com>
2026-06-01 11:08:39 +0200

    Intensify pastel prompt colors

--- src/components/ChordAndNoteDegree.tsx
+++ src/components/ChordAndNoteDegree.tsx
@@ -11,12 +11,12 @@ type Props = {
 };
 
 const chordColorMapping: { [chord in Chord]: string } = {
-  I: "text-sky-200/90",
-  ii: "text-sky-200/80",
-  iii: "text-sky-200/70",
-  IV: "text-sky-100/90",
-  V: "text-sky-100/80",
-  vi: "text-sky-100/70",
+  I: "text-sky-300",
+  ii: "text-sky-300/90",
+  iii: "text-sky-300/80",
+  IV: "text-sky-200",
+  V: "text-sky-200/90",
+  vi: "text-sky-200/80",
 };
 
 const colorChordProgression = (chordsProgression: ChordsProgression) => {
@@ -41,9 +41,9 @@ export const ChordAndNoteDegree = ({
   return (
     <div className="flex min-h-80 flex-col items-center justify-center gap-12 px-2 py-10">
       <h1 className="text-center text-5xl font-black tracking-normal text-white sm:text-7xl lg:text-8xl">
-        {position && <span className="text-amber-200/90">{position} </span>}
-        {chordDegree && <span className="text-sky-200/90">{chordDegree} </span>}
-        {noteDegree && <span className="text-rose-200/90">{noteDegree} </span>}
+        {position && <span className="text-amber-300">{position} </span>}
+        {chordDegree && <span className="text-sky-300">{chordDegree} </span>}
+        {noteDegree && <span className="text-rose-300">{noteDegree} </span>}
         {chordsProgression && colorChordProgression(chordsProgression)}
       </h1>
     </div>
